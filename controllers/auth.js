23e7c930e889b442eecb350b78006ac513bc882e
kartikpavan <kartik.varanasi@yahoo.com>
2023-02-10 15:14:28 +0530

    Mongoose middleware password hashing

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,10 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+
 const register = async (req, res) => {
-   res.send("Register Route");
+   const user = await User.create({ ...req.body });
+   res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+   name: {
+      type: String,
+      required: [true, "Please provide a name"],
+      minlength: 3,
+      maxlength: 50,
+   },
+   email: {
+      type: String,
+      required: [true, "Please Provide an Email"],
+      match: [
+         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+         "Invalid EMAIl",
+      ],
+      unique: true,
+   },
+   password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: 6,
+   },
+});
+
+UserSchema.pre("save", async function (next) {
+   const salt = await bcrypt.genSalt(10);
+   this.password = await bcrypt.hash(this.password, salt); // here this refers to the User Document
+   next();
+});
+
+module.exports = mongoose.model("User", UserSchema);
